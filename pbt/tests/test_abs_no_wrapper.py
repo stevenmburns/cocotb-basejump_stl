6c0705fa601ef09ffb8cc3a2c8bd09ae5f7c74e1
Steve Burns <steven.m.burns@intel.com>
2022-11-28 16:52:40 -0800

    Add broken test

---
+++ pbt/tests/test_abs_no_wrapper.py
@@ -0,0 +1,19 @@
+from cocotb_test.run import run
+from hypothesis import given, example, strategies as st, settings
+import pathlib
+
+testdir = pathlib.Path(__file__).parent
+rootdir = pathlib.Path(__file__).parent.parent.parent
+
+@settings(deadline=300000,max_examples=20)
+@given(st.integers(min_value=2,max_value=10))
+@example(8)
+def test_abs_verilog(width_p):
+    print(f'Running with width_p={width_p}...')
+    run(verilog_sources=[str(rootdir / "basejump_stl/bsg_misc/bsg_abs.v")],
+        includes=[str(rootdir / "basejump_stl/bsg_misc")],
+        toplevel="bsg_abs",
+        module="abs_cocotb",
+        #extra_args=["-Wno-fatal", f"-pvalue+width_p={width_p}"]
+        extra_args=[f"-pvalue+width_p={width_p}"]
+    )
